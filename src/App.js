29469ed7324864aac0ea07079d7bc5902e3aa208
codewithmeyouknowigotit <dastl7.dev@gmail.com>
2024-03-03 18:13:59 -0500

    Intro to props and useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Employee from "./components/Employee";
+import { useState } from "react";
 
 function App() {
+  // How to use state
+  const [role, setRole] = useState("dev");
+
+  // let role = "dev";
+
   // Logic can be added here before the return statement
-  console.log("We are about the lost teh employees");
-  const showEmployees = false;
+  console.log("We are about the list the employees");
+  const showEmployees = true;
 
   return (
     // 1. Only 1 div can be allowed here
@@ -14,9 +20,17 @@ function App() {
       {/* This is a tenary operator */}
       {showEmployees ? (
         <>
-          <Employee />
-          <Employee />
-          <Employee />
+          <input
+            type="text"
+            onChange={(e) => {
+              // To get what is typed in the input
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
+          <Employee name="Jerome" role="Intern" />
+          <Employee name="Nicole" role={role} />
+          <Employee name="John" />
         </>
       ) : (
         <p>You cannot see the employees</p>
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,5 +1,14 @@
-function Employee() {
-  return <h3>Here is an employee</h3>;
+function Employee(props) {
+  // Do not set values to props. Code below sets an error and crashes the site
+  // props.name = "something";
+  return (
+    // Do no set values to probs. That should not be done in the child component
+    // only in the parent component
+    <>
+      <h3>Employee {props.name}</h3>
+      <p>{props.role ? props.role : "No role"}</p>
+    </>
+  );
 }
 
 export default Employee;
