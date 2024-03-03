34f31b4816056ddace5ae91795540e1009a88d24
codewithmeyouknowigotit <dastl7.dev@gmail.com>
2024-03-03 17:42:01 -0500

    Create my first component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import "./App.css";
+import Employee from "./components/Employee";
 
 function App() {
+  // Logic can be added here before the return statement
+  console.log("We are about the lost teh employees");
+  const showEmployees = false;
+
   return (
+    // 1. Only 1 div can be allowed here
+    // 2. Logic can then be implemented in the div below
+
     <div className="App">
-      <header className="App-header">
-        <p>Hey</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* This is a tenary operator */}
+      {showEmployees ? (
+        <>
+          <Employee />
+          <Employee />
+          <Employee />
+        </>
+      ) : (
+        <p>You cannot see the employees</p>
+      )}
     </div>
   );
 }
