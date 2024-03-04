920904295609907eb28c344def3359f7ed2d08f5
codewithmeyouknowigotit <dastl7.dev@gmail.com>
2024-03-03 19:00:50 -0500

    Intro to TailwindCSS

--- src/App.js
+++ src/App.js
@@ -6,31 +6,71 @@ function App() {
   // How to use state
   const [role, setRole] = useState("dev");
 
-  // let role = "dev";
-
   // Logic can be added here before the return statement
   console.log("We are about the list the employees");
   const showEmployees = true;
 
   return (
-    // 1. Only 1 div can be allowed here
     // 2. Logic can then be implemented in the div below
 
-    <div className="App bg-red-300">
+    <div className="App">
       {/* This is a tenary operator */}
       {showEmployees ? (
         <>
-          <input
+          {/* <input
             type="text"
             onChange={(e) => {
               // To get what is typed in the input
               console.log(e.target.value);
               setRole(e.target.value);
             }}
-          />
-          <Employee name="Jerome" role="Intern" />
-          <Employee name="Nicole" role={role} />
-          <Employee name="John" />
+          /> */}
+          <div className="flex flex-wrap justify-center">
+            <Employee
+              name="Jerome"
+              role="Intern"
+              img="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            />
+            <Employee
+              name="Nicole"
+              role={role}
+              img="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            />
+            <Employee
+              name="John"
+              img="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            />
+
+            <Employee
+              name="Jerome"
+              role="Intern"
+              img="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            />
+            <Employee
+              name="Nicole"
+              role={role}
+              img="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            />
+            <Employee
+              name="John"
+              img="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            />
+
+            <Employee
+              name="Jerome"
+              role="Intern"
+              img="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            />
+            <Employee
+              name="Nicole"
+              role={role}
+              img="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            />
+            <Employee
+              name="John"
+              img="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            />
+          </div>
         </>
       ) : (
         <p>You cannot see the employees</p>
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,13 +1,20 @@
 function Employee(props) {
-  // Do not set values to props. Code below sets an error and crashes the site
-  // props.name = "something";
   return (
-    // Do no set values to probs. That should not be done in the child component
-    // only in the parent component
-    <>
-      <h3>Employee {props.name}</h3>
-      <p>{props.role ? props.role : "No role"}</p>
-    </>
+    <div class="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <img
+        class="object-cover rounded-full h-[100px] w-[100px] block mx-auto sm:mx-0 sm:shrink-0"
+        src={props.img}
+      />
+      <div className="text-center space-y-2 sm:text-left">
+        <div className="space-y-0.5">
+          <p className="text-lg text-black font-semibold">{props.name}</p>
+          <p className="text-slate-500 font-medium">{props.role}</p>
+        </div>
+        <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+          Message
+        </button>
+      </div>
+    </div>
   );
 }
 
